d0c6143d0748b50b353e6623936daabd2c89abbe
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-28 15:46:09 -0400

    Add project files.

---
+++ TACsharpExtrasDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TACsharpExtrasDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
